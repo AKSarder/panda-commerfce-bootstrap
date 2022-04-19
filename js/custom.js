2d7fe137d0863f7f0c067015858870eb045c6267
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 16:33:13 +0600

    done

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+document.getElementById("shoes-header").style.color = "yellow";
+
+document.getElementById("backpack").style.backgroundColor = 'hotpink';
+
+
+// card ar border radius change 
+const card = document.getElementsByClassName("card");
+
+
+
+
+
